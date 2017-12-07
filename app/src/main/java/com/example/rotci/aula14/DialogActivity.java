e3f38fdf149b251813a19f23148f2839f2215690
RotcivOcnarb <rotciv_ocnarb@hotmail.com>
2017-12-07 16:20:16 -0200

    Initial commit

---
+++ app/src/main/java/com/example/rotci/aula14/DialogActivity.java
@@ -0,0 +1,21 @@
+package com.example.rotci.aula14;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class DialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_dialog);
+    }
+
+    public void finishDialog(View v) {
+        this.finish();
+    }
+}
